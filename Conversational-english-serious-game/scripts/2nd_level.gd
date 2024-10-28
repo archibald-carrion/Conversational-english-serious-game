e61b9a6345dd4e588de9b4e36444c855a02f5aa6
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-10-28 10:14:40 -0600

    add new version of level 02

---
+++ Conversational-english-serious-game/scripts/2nd_level.gd
@@ -0,0 +1,102 @@
+var questions_level_2 = [
+	{
+		"question": "What is the main challenge\nyou face in your job?",
+		"answers": [
+			"Communicating in different languages\nwith my clients is a constant\nstruggle for me.",
+			"Commuting becomes a daily problem\nas I spend more time in my car\nthan in the office.",
+			"Prioritizing tasks and setting deadlines\nto manage my time appropriately\nis really difficult for me."
+		],
+		"correct_answer": 2,
+		"audio_file" : "res://assets/audio/diff_02/1.mp3"
+	},
+	{
+		"question": "How has your role in the\ncompany changed over time?",
+		"answers": [
+			"My responsibilities have been\nconsistent since the my first\nday working here.",
+			"I'm now working part-time instead\nof full-time as I needed to\nfinish my studies.",
+			"At the beginning I was in an\nentry-level position, but now I am\nworking as a manager."
+		],
+		"correct_answer": 2,
+		"audio_file" : "res://assets/audio/diff_02/2.mp3"
+	},
+	{
+		"question": "What motivates you to keep\nimproving in your current occupation?",
+		"answers": [
+			"I enjoy the routine of my daily\nwork, so I stay motivated.",
+			"I am motivated by continuously\nlearning new technologies.",
+			"How my colleagues inspire me\nevery day is my main motivation."
+		],
+		"correct_answer": 1,
+		"audio_file" : "res://assets/audio/diff_02/3.mp3"
+	},
+	{
+		"question": "How do you manage stress\nand pressure at work?",
+		"answers": [
+			"I try to stay organized and take\nregular breaks to maintain a\nwork-life balance.",
+			"Talking to my friends and family\nabout my challenges works for me.",
+			"Mindfulness and exercising have\nhelped me in this kind of situation."
+		],
+		"correct_answer": 0,
+		"audio_file" : "res://assets/audio/diff_02/4.mp3"
+	},
+	{
+		"question": "What are the most important\nqualities to succeed in\nyour industry?",
+		"answers": [
+			"Creativity and innovation are\nimportant in my field.",
+			"Adaptability, teamwork, and\nproblem-solving are crucial.",
+			"Organization and punctuality\nare key in any job."
+		],
+		"correct_answer": 1,
+		"audio_file" : "res://assets/audio/diff_02/5.mp3"
+	},
+	{
+		"question": "What kind of impact do you hope\nyour work will have on the company\nand its future growth?",
+		"answers": [
+			"I hope my work will help the\ncompany reach new clients.",
+			"I hope my work will contribute to\nthe company's expansion into new markets.",
+			"I hope my work will improve\ncommunication between departments."
+		],
+		"correct_answer": 1,
+		"audio_file" : "res://assets/audio/diff_02/6.mp3"
+	},
+	{
+		"question": "How do you keep yourself updated\nwith the latest trends and\nadvancements in your field?",
+		"answers": [
+			"I regularly attend conferences\nand take online courses.",
+			"Watching the news every morning\nhelps me keep updated.",
+			"I learn new trends by following\nprofessionals on social media."
+		],
+		"correct_answer": 0,
+		"audio_file" : "res://assets/audio/diff_02/7.mp3"
+	},
+	{
+		"question": "How does technology help you\nimprove your job performance?",
+		"answers": [
+			"Technology plays a key role by\nstreamlining processes and\nautomating tasks.",
+			"Technology plays a key role by\nfacilitating communication with\ninternational teams.",
+			"Technology plays a key role by\nallowing the participation in\ninternational trainings online."
+		],
+		"correct_answer": 0,
+		"audio_file" : "res://assets/audio/diff_02/8.mp3"
+	},
+	{
+		"question": "How does teamwork contribute\nto achieving the company's goals?",
+		"answers": [
+			"It combines expertise to meet\nobjectives through clear communication.",
+			"The main factor of teamwork is\nsharing ideas to advance efficiently.",
+			"Motivating team members to work\ntogether is a key contribution."
+		],
+		"correct_answer": 0,
+		"audio_file" : "res://assets/audio/diff_02/9.mp3"
+	},
+	{
+		"question": "What advice would you give to\nsomeone starting a career\nin your field?",
+		"answers": [
+			"I'd suggest building strong\nrelationships with colleagues.",
+			"They should work on being patient\nand open to learning.",
+			"I'd recommend focusing on technical\nskills and networking."
+		],
+		"correct_answer": 2,
+		"audio_file" : "res://assets/audio/diff_02/10.mp3"
+	}
+];
